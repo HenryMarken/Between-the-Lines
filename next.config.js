953affa1c771e552c72b09ba272202697d51d658
HenryMarken <hmarken@ualberta.ca>
2023-02-09 19:59:09 -0700

    search bar using google books api

--- next.config.js
+++ next.config.js
@@ -4,3 +4,17 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+module.exports = {
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'assets.vercel.com',
+        port: '',
+        pathname: '/image/upload/**',
+      },
+    ],
+  },
+}
